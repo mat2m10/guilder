6d551ccd5046c2c1dbc842404cd2f8c073fcee53
Matthieu de Hemptinne <matthieu.dehemptinne@gmail.com>
2020-11-09 15:48:43 +0100

    Crud Branch created for booking and crafts model and controller + routes

--- app/controllers/crafts_controller.rb
+++ app/controllers/crafts_controller.rb
@@ -1,23 +1,41 @@
 class CraftsController < ApplicationController
   def index
+    @crafts = Craft.all
   end
 
   def show
-  end
-
-  def create
-  end
-
-  def update
+    @craft = Craft.find(params[:id])
   end
 
   def new
+    @craft = Craft.new
+  end
+
+  def create
+    @craft = Craft.new(craft_params)
+    @craft.save
+    redirect_to craft_path(@craft)
   end
 
   def edit
+    @craft = Craft.find(params[:id])
+  end
+
+  def update
+    @craft = Craft.find(params[:id])
+    @craft.update(params[:craft])
   end
 
   def destroy
+    @craft = Craft.find(params[:id])
+    @craft.destroy
+    redirect_to crafts_path
+  end
+
+  private
+
+  def craft_params
+    params.require(:craft).permit(:name, :price)
   end
 
 end
