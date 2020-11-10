90b7a1b10f324eedcad47909176a809c8f9dc3a5
Matthieu de Hemptinne <matthieu.dehemptinne@gmail.com>
2020-11-10 21:21:05 +0100

    Navbar logo

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
   resources :users, only: [:show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
