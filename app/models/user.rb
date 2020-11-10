6dcff164f49a199f370e434c621e747018872f96
Matthieu de Hemptinne <matthieu.dehemptinne@gmail.com>
2020-11-10 13:45:11 +0100

    signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :booked_crafts, through: :bookings, source: :craft #User qui ne sont pas des craftsmen
   has_many :crafts
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
