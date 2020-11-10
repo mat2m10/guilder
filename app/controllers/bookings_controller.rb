43cb754098f1ad0072f2ec9a6a0fea96fafa3d5e
Aurelie Rousseau <rousseau.au@gmail.com>
2020-11-10 12:35:50 +0100

    some features for booking a crafft/service with a form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,18 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @craft = Craft.find(params[:craft_id])
   end
 
   def create
-    @booking = Booking.new(craft_params)
-    @booking.save
-    redirect_to booking_path(@booking)
+    @booking = Booking.new(bookings_params)
+    @craft = Craft.find(params[:craft_id])
+    @booking.craft = @craft
+    @booking.save    
+    redirect_to crafts_path
+  end
+
+  def bookings_params
+    params.require(:booking).permit(:date, :duration)
   end
 end
