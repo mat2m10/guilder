1464b2807b842c353e6f949de0f06ae6da117abe
Pierre Lion <pierrelion1997@yahoo.fr>
2020-11-11 11:34:59 +0100

    A: changed pundit B: updated view with buttons (yes theyre moche) C: also ran test with price very long and its kind of ok

--- app/policies/craft_policy.rb
+++ app/policies/craft_policy.rb
@@ -8,11 +8,11 @@ class CraftPolicy < ApplicationPolicy
   end
 
   def update?
-    true
+    record.user == user
   end
 
   def destroy?
-    true
+    record.user == user
   end
 
   class Scope < Scope
