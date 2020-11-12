2faaec98af639d13c69e13c48d5986a8ead165d6
Nicolas Eloy <colasrsca@gmail.com>
2020-11-12 18:26:59 +0100

    Homepage search form PG Germ added

--- app/models/craft.rb
+++ app/models/craft.rb
@@ -5,4 +5,13 @@ class Craft < ApplicationRecord
   has_one_attached :photo
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_name
+
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true } # <-- now it will return something!
+    }
 end
