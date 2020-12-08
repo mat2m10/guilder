cf6b7e8557a32481b05d77747b38add1dd0232d0
Raphael Marsan <marsanraphael@gmail.com>
2020-12-08 17:30:38 +0100

    removed devise authentication for crafts index, show, new, create

--- app/controllers/crafts_controller.rb
+++ app/controllers/crafts_controller.rb
@@ -1,5 +1,6 @@
 class CraftsController < ApplicationController
   before_action :set_craft, only: %i[edit update show destroy]
+  skip_before_action :authenticate_user!, only: [ :index, :show, :new, :create ]
 
   def index
     @users = User.all
