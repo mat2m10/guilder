e32c71c7942ee77f98f08e82cc0e08a03e02b4da
Raphael Marsan <marsanraphael@gmail.com>
2020-12-22 11:54:41 +0100

    added availability features

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_104541) do
+ActiveRecord::Schema.define(version: 2020_12_22_105044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2020_11_12_104541) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
+    t.date "end_date"
+    t.date "start_date"
     t.index ["craft_id"], name: "index_bookings_on_craft_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
